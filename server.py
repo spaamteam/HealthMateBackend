5935e894f99219c9485b209198bbddc082b3d64e
saurabhpatil8989 <saurabhpatil2007@gmail.com>
2016-03-27 12:52:38 -0500

    correction

--- server.py
+++ server.py
@@ -139,4 +139,4 @@ def create_db_conn(coll_name):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host='localhost', port=port)
+    app.run(debug=True, host='0.0.0.0', port=port)

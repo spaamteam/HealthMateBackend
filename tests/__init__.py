369b90e1edd3eea3b27a7456f8a6b2fa64d34f08
Aditya Purandare <aditya.p1993@hotmail.com>
2016-03-26 20:29:44 -0500

    Rename server

---
+++ tests/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import unittest
+
+class InitializationTests(unittest.TestCase):
+
+    def test_initialization(self):
+        """
+        Check the test suite runs by affirming 2+2=4
+        """
+        self.assertEqual(2+2, 4)
+
+    def test_patient_login(self):
+        """
+        Checks for object creation for class in Problem 1
+        """
+        from server import *
+        

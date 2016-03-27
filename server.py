bad6947f85fd7faef645e2481470b531cbe77154
Aditya Purandare <aditya.p1993@hotmail.com>
2016-03-26 20:36:40 -0500

    Add port for heroku

--- server.py
+++ server.py
@@ -83,4 +83,5 @@ def create_db_conn(coll_name):
     return db.get_collection(coll_name)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=2020)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
